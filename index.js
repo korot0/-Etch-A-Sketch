780ced9fee1169ae29b85ce5797455ed931450e5
korot0 <108902180+korot0@users.noreply.github.com>
2022-11-12 22:47:38 -0600

    Generate new grid based on the range slider

--- index.js
+++ index.js
@@ -1,45 +1,59 @@
 // do white and dark mode
 // no right click
-// can you use toggle anywhere?
-// make it work only when you're holding the mouse 
+// cubeta
 
 const gridContainer = document.getElementById('grid-container');
-const reloadBtn = document.querySelector('button');
-
-function createSquares() {
-    //CREATE SQUARES
-    const divs = document.createElement('div');
-    divs.setAttribute('style', 'width: 35px; height: 35px; border: 1px solid gray;');
-    gridContainer.appendChild(divs);
-
-    //DRAW--------------------------------------------------------------------------
-    let mouseDown = false;
-    gridContainer.addEventListener('mousedown', () => mouseDown = true);
-    gridContainer.addEventListener('mouseup', () => mouseDown = false);
-    gridContainer.addEventListener('mouseleave', () => mouseDown = false);
-    divs.addEventListener('mousemove', () => {
-        if(mouseDown) divs.style.backgroundColor = 'black';
-    });
-    divs.addEventListener('mousedown', () => {
-         divs.style.backgroundColor = 'black';
-    });
-
-    //ERASE-------------------------------------------------------------------------
-    reloadBtn.addEventListener('click', () => {
-        divs.style.backgroundColor = 'bisque';
-    });
-}
-
-for (let i = 1; i < 257; i++) {
-    createSquares();
-}
+const eraseBtn = document.getElementById('erase');
 
 // SLIDER GRID SIZE TEXT OUTPUT
 const sliderEl = document.getElementById('sliderEl');
 const sliderOutput = document.getElementById('sizeTextEl');
-sliderOutput.innerHTML = sliderEl.value;
-
-sliderEl.oninput = function (){
-    sliderOutput.innerHTML = this.value;
+sliderOutput.textContent = sliderEl.value + ' x ' + sliderEl.value;
+sliderEl.oninput = function() {
+    sliderOutput.textContent = this.value + ' x ' + this.value;
 }
+
+//HEIGHT AND WIDTH OF DIVS INSIDE GRID
+let gridSize = sliderEl.value * sliderEl.value;
+let widthAndHeight = 650 / sliderEl.value;
+
+//write an explanation here... cuz I already forgot wtf is going on
+let mouseDown = false;
+gridContainer.onmousedown = () => mouseDown = true;
+gridContainer.onmouseup = () => mouseDown = false;
+gridContainer.onmouseleave = () => mouseDown = false;
+
+//GRID MAKER (GIVE AN EXPLANATION OF WHAT IS GOING ON HERE!)
+function grid() {
+    for (let i = 1; i <= gridSize; i++) {
+        //CREATE SQUARES
+        const divs = document.createElement('div');
+        divs.style.border = '1px solid gray';
+        divs.style.width = `${widthAndHeight}px`;
+        divs.style.height = `${widthAndHeight}px`;
+        gridContainer.appendChild(divs);
+    
+        //DRAW------------------------------------------------------------------------
+        divs.addEventListener('mousemove', () => {
+            if(mouseDown) divs.style.backgroundColor = 'black';
+        });
+        divs.addEventListener('mousedown', () => {
+             divs.style.backgroundColor = 'black';
+        });
+    
+        // ERASE----------------------------------------------------------------------
+        eraseBtn.addEventListener('click', () => {
+            divs.style.backgroundColor = 'bisque';
+        });
+    }
+}
+
+grid();
+
+document.querySelector('input').addEventListener('change', e => {
+    gridContainer.innerHTML = '';
+    gridSize = e.target.value * e.target.value;
+    widthAndHeight = 650 / e.target.value;
+    grid();
+});
 
