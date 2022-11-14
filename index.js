d82a03b39ae457820fc5e0beb9e9ba61ae234191
korot0 <108902180+korot0@users.noreply.github.com>
2022-11-14 13:17:29 -0600

    Change functional buttons to toggle buttons

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // do white and dark mode
-// no right click
 // cubeta
 
 const gridContainer = document.getElementById('grid-container');
 const eraseBtn = document.getElementById('erase');
+const randomColorBtn = document.getElementById('randomColor');
 
 // SLIDER GRID SIZE TEXT OUTPUT
 const sliderEl = document.getElementById('sliderEl');
@@ -17,24 +17,42 @@ sliderEl.oninput = function() {
 let gridSize = sliderEl.value * sliderEl.value;
 let widthAndHeight = 650 / sliderEl.value;
 
+//GIVE EXPLANATION HERE AS WELL!
+document.querySelector('input').addEventListener('change', e => {
+    gridContainer.innerHTML = '';
+    gridSize = e.target.value * e.target.value;
+    widthAndHeight = 650 / e.target.value;
+    grid();
+});
+
 //write an explanation here... cuz I already forgot wtf is going on
 let mouseDown = false;
 gridContainer.onmousedown = () => mouseDown = true;
 gridContainer.onmouseup = () => mouseDown = false;
 gridContainer.onmouseleave = () => mouseDown = false;
 
+//To select random color
+function getRandomColor() {
+    let arrayRandomColor = [];
+    for (let i = 0; i < 3; i++) {
+        arrayRandomColor.push(Math.floor(Math.random() * 256))
+    }
+    return 'rgb' + `(${arrayRandomColor.toString()})`;
+}
+
+
 //GRID MAKER (GIVE AN EXPLANATION OF WHAT IS GOING ON HERE!)
 function grid() {
     for (let i = 1; i <= gridSize; i++) {
         //CREATE SQUARES
         const divs = document.createElement('div');
-        divs.style.border = '1px solid gray';
+        // divs.style.border = '1px solid gray';
         divs.style.width = `${widthAndHeight}px`;
         divs.style.height = `${widthAndHeight}px`;
         gridContainer.appendChild(divs);
     
         //DRAW------------------------------------------------------------------------
-        divs.addEventListener('mousemove', () => {
+        divs.addEventListener('mouseover', () => {
             if(mouseDown) divs.style.backgroundColor = 'black';
         });
         divs.addEventListener('mousedown', () => {
@@ -43,17 +61,8 @@ function grid() {
     
         // ERASE----------------------------------------------------------------------
         eraseBtn.addEventListener('click', () => {
-            divs.style.backgroundColor = 'bisque';
+            divs.style.backgroundColor = '#efefef';
         });
     }
 }
-
 grid();
-
-document.querySelector('input').addEventListener('change', e => {
-    gridContainer.innerHTML = '';
-    gridSize = e.target.value * e.target.value;
-    widthAndHeight = 650 / e.target.value;
-    grid();
-});
-
