21ed611fd9b3cf9d875532140e3fcf2f7b91d306
korot0 <108902180+korot0@users.noreply.github.com>
2022-11-26 22:42:29 -0600

    Final refactoring

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-//TO DO -------------------->
 // Fix canvas alignment with header
-// Fix clear button and ask why is it slow with certain functions
-// Fix eraser button
-// Fix footer scaling
-// Canvas color
 
 const gridContainer = document.getElementById('grid-container');
 //BUTTON ELEMENTS
@@ -16,24 +11,28 @@ const randomColorBtn = document.getElementById('randomColorBtn-el');
 //SLIDER OUTPUT E.G 'Grid Size: 5 x 5' 
 const sliderEl = document.getElementById('sliderEl');
 const sliderOutput = document.getElementById('sizeTextEl');
-
 sliderOutput.textContent = sliderEl.value + ' x ' + sliderEl.value;
 sliderEl.oninput = function() {
     sliderOutput.textContent = this.value + ' x ' + this.value;
 }
 
 //HEIGHT AND WIDTH OF DIVS INSIDE GRID
-let widthAndHeight = 650 / sliderEl.value;
-//This function changes the grid size. First it clears the previous grid. Then it changes de grid size depending on the current value (e.target.value) of the slider by multiplying e.g 16 x 16 = 256 number of squares. Then it dictates the width and height of each square by diving the .... KEEP EXPLAINING WHY DIVIDE 650px?
+let widthAndHeight = 550 / sliderEl.value;
+//This function changes the grid size. First it clears the previous grid. Then it changes de grid size depending on the current value (e.target.value) of the slider by multiplying e.g 16 x 16 = 256 number of squares. Then it dictates the width and height of each square by diving the .... KEEP EXPLAINING WHY DIVIDE 550px?
 document.querySelector('input').addEventListener('change', e => {
     gridContainer.innerHTML = '';
-    widthAndHeight = 650 / e.target.value;
+    widthAndHeight = 550 / e.target.value;
     createGrid(e.target.value, e.target.value);
     draw();
     showGrid();
-    eraser();
-    clear();
 });
+
+//CANVAS COLOR
+const canvasColorPicker = document.getElementById('canvasColorPicker-el');
+canvasColorPicker.oninput = () => {
+    canvasColor = canvasColorPicker.value;
+    gridContainer.style.backgroundColor = canvasColor;
+}
 
 //GRID MAKER (GIVE AN EXPLANATION OF WHAT IS GOING ON HERE!)
 function createGrid(columns, rows) {
@@ -61,16 +60,6 @@ function showGrid() {
 }
 showGrid();
 
-//CLEAR
-function clear() {
-    document.querySelectorAll('.divs').forEach((divEl) => {
-        clearBtn.addEventListener('click', () => {
-            divEl.style.removeProperty('background-color');
-        });
-    });
-}
-clear();
-
 //DRAWING EXPLAIN!!!
 //In order to draw only when clicking and holding, not when hovering, here is what happens: When in createGrid() mouseover activates when hovering through an element. KEEP EXPLAINING...
 let mouseDown = false;
@@ -83,7 +72,10 @@ function draw() {
     document.querySelectorAll('.divs').forEach((divEl) => {
         let opacity = 0.1;
         divEl.addEventListener('mouseover', () => {
-            if (mouseDown && randomColorBtn.checked) {
+            if (mouseDown && eraserBtn.checked) {
+                opacity = 0.1;
+                divEl.style.removeProperty('background-color');
+            } else if (mouseDown && randomColorBtn.checked) {
                 divEl.style.backgroundColor = getRandomColor();
             } else if (mouseDown && shadeBtn.checked) {
                 opacity += 0.1;
@@ -94,7 +86,10 @@ function draw() {
             }
         });
         divEl.addEventListener('click', () => {
-            if (randomColorBtn.checked ) {
+            if (eraserBtn.checked) {
+                opacity = 0.1;
+                divEl.style.removeProperty('background-color');
+            } else if (randomColorBtn.checked ) {
                 divEl.style.backgroundColor = getRandomColor() 
             } else if (shadeBtn.checked) {
                 opacity += 0.1;
@@ -104,33 +99,22 @@ function draw() {
                 divEl.style.backgroundColor = color;
             }
         });
+        //CLEAR BUTTON
+        clearBtn.addEventListener('click', () => {
+            opacity = 0.1;
+            divEl.style.removeProperty('background-color');
+        });
     });
 }
 draw();
 
-//ERASER
-function eraser() {
-    document.querySelectorAll('.divs').forEach((divEl) => {
-        divEl.addEventListener('mouseover', () => {
-            if (mouseDown && eraserBtn.checked) {
-                divEl.style.removeProperty('background-color');
-            }
-        });
-        divEl.addEventListener('click', () => {
-            if (eraserBtn.checked) divEl.style.removeProperty('background-color'); 
-        });
-    });
-}
-eraser();
-
 //COLOR PICKER FUNCTION
-const colorPickerBtn = document.getElementById('colorPickerBtn-el');
 let color = '#000000';
+const colorPickerBtn = document.getElementById('colorPickerBtn-el');
 colorPickerBtn.oninput = () => {
     color = colorPickerBtn.value;
-    // document.getElementById('color-code').textContent = colorPickerBtn.value;
-    // HTML -> <span id="color-code">#000000</span>
-} 
+}
+
 //HEX TO RGB FUNCTION
 function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
@@ -157,7 +141,6 @@ function getRandomColor() {
     }
     return `rgb(${arrayRandomColor})`;
 }
-
 // NOTES --------------------------------------------------------->
 
 //BIG LESSON LEARNED HERE!!! REMEMBER THE BIG ASS FUNCTION WE HAD? THAT WAS AVOIDED BY USING '.classList.add('divs'); ON 'gridContainer.appendChild(divs)' NOW WE CAN ACCESS THE DIVS INSIDE THE createGrid() FUNCTION IN THE GLOBAL SCOPE BY TARGETING THE '.divs'. REFER TO *
